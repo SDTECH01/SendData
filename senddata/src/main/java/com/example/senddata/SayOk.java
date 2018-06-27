5d343682653a8cd776ba47ac7799f60024b42614
SDTECH01 <smartdatatechnologie@gmail.com>
2018-06-27 10:15:53 +0000

    first send Data

---
+++ senddata/src/main/java/com/example/senddata/SayOk.java
@@ -0,0 +1,11 @@
+package com.example.senddata;
+
+import android.widget.Toast;
+
+public class SayOk {
+    public static String SaysOk(){
+        //Toast.makeText(,"OK",Toast.LENGTH_LONG).show();
+        String vari =  "Ok";
+        return vari;
+    }
+}
